a39b1d559e6d1c1f0c4c7bf4534de6f153a35eb8
MATALON Luan <luan.matalon@brassart.fr>
2024-05-15 02:36:49 +0200

    AjoutAnimationPerso+TilesBackground
    Rajouter le background et construire le Niveau
    
    Si j'ai le temps faire un boss

--- Assets/MetroidvaniaController/Scripts/Player/Attack.cs
+++ Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -4,67 +4,66 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-	public float dmgValue = 4;
-	public GameObject throwableObject;
-	public Transform attackCheck;
-	private Rigidbody2D m_Rigidbody2D;
-	public Animator animator;
-	public bool canAttack = true;
-	public bool isTimeToCheck = false;
+    public float dmgValue = 4;
+    public GameObject throwableObject;
+    public Transform attackCheck;
+    private Rigidbody2D m_Rigidbody2D;
+    public Animator animator;
+    public bool canAttack = true;
 
-	public GameObject cam;
+    public GameObject cam;
 
-	private void Awake()
-	{
-		m_Rigidbody2D = GetComponent<Rigidbody2D>();
-	}
+    private BoxCollider2D attackHitbox;  // Collider for the attack hitbox
 
-	// Start is called before the first frame update
-	void Start()
+    private void Awake()
     {
-        
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        attackHitbox = GetComponent<BoxCollider2D>();  // Ensure this is added in the Inspector
+        attackHitbox.enabled = false;  // Disable hitbox by default
     }
 
-    // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.X) && canAttack)
-		{
-			canAttack = false;
-			animator.SetBool("IsAttacking", true);
-			StartCoroutine(AttackCooldown());
-		}
+        if (Input.GetKeyDown(KeyCode.X) && canAttack)
+        {
+            canAttack = false;
+            animator.SetBool("IsAttacking", true);
+            StartCoroutine(AttackCooldown());
+        }
 
-		if (Input.GetKeyDown(KeyCode.V))
-		{
-			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
-			Vector2 direction = new Vector2(transform.localScale.x, 0);
-			throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
-			throwableWeapon.name = "ThrowableWeapon";
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f, -0.2f), Quaternion.identity) as GameObject;
+            Vector2 direction = new Vector2(transform.localScale.x, 0);
+            throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction;
+            throwableWeapon.name = "ThrowableWeapon";
+        }
+    }
 
-	IEnumerator AttackCooldown()
-	{
-		yield return new WaitForSeconds(0.25f);
-		canAttack = true;
-	}
+    IEnumerator AttackCooldown()
+    {
+        ActivateHitbox();
+        yield return new WaitForSeconds(0.25f);
+        DeactivateHitbox();
+        canAttack = true;
+    }
 
-	public void DoDashDamage()
-	{
-		dmgValue = Mathf.Abs(dmgValue);
-		Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
-		for (int i = 0; i < collidersEnemies.Length; i++)
-		{
-			if (collidersEnemies[i].gameObject.tag == "Enemy")
-			{
-				if (collidersEnemies[i].transform.position.x - transform.position.x < 0)
-				{
-					dmgValue = -dmgValue;
-				}
-				collidersEnemies[i].gameObject.SendMessage("ApplyDamage", dmgValue);
-				cam.GetComponent<CameraFollow>().ShakeCamera();
-			}
-		}
-	}
+    private void ActivateHitbox()
+    {
+        attackHitbox.enabled = true;
+    }
+
+    private void DeactivateHitbox()
+    {
+        attackHitbox.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.SendMessage("ApplyDamage", dmgValue);
+            cam.GetComponent<CameraFollow>().ShakeCamera();
+        }
+    }
 }
